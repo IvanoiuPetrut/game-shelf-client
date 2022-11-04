0f0a125f45e301cc3f84fda205540e13642e6fe0
Petrut <ivanoiu.petrut27@yahoo.com>
2022-11-04 23:00:01 +0200

    feat: add game detail fetch from api The component recives as an prop the game ID that will be use as an URL argument for fetching the details of the game

---
+++ src/components/GameItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p>{{ gameName }}</p>
+    <img :src="gameImage" alt="game image" width="200" />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import { onBeforeMount } from "vue";
+import axios from "axios";
+
+export default {
+  name: "GameItem",
+  props: {
+    gameId: String,
+  },
+  setup(props) {
+    const gameName = ref("Fallout 3");
+    const gameImage = ref(
+      "https://images.unsplash.com/photo-1612820685478-301051f03dd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1322&q=80"
+    );
+
+    const gameUrl = `https://api.rawg.io/api/games/${props.gameId}`;
+    const gameApiKey = "f062f25bd9424cb6905d4ce655e4e583";
+
+    onBeforeMount(() => {
+      axios
+        .get(gameUrl, {
+          params: {
+            key: gameApiKey,
+          },
+        })
+        .then((response) => {
+          gameName.value = response.data.name;
+          gameImage.value = response.data.background_image;
+        });
+    });
+
+    return {
+      gameName,
+      gameImage,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+import GameItem from "../components/GameItem.vue";
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <GameItem :game-id="'187'" />
   </main>
 </template>
