bb9fe073f73d3fc97b5d34ee727b2bc4782af041
Petrut <ivanoiu.petrut27@yahoo.com>
2022-11-06 12:36:30 +0200

    fix: removed the single GameItem component from HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from "../components/TheWelcome.vue";
-import GameItem from "../components/GameItem.vue";
 import GamesScroller from "../components/GamesScroller.vue";
 </script>
 
 <template>
   <main>
     <TheWelcome />
-    <GameItem :game-id="'187'" />
     <GamesScroller />
   </main>
 </template>
