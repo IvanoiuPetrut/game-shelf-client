2d1555cadeaed819c13f48fc28b6bce19b7a084d
Petrut <ivanoiu.petrut27@yahoo.com>
2022-11-06 12:43:25 +0200

    fix: removed TheWelcome component from file structure and from HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
 import GamesScroller from "../components/GamesScroller.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <GamesScroller />
   </main>
 </template>
