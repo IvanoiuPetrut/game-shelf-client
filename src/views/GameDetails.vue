dfe6baea2b1ef958b0640fa7bb8d34aa94a8c66a
Petrut <ivanoiu.petrut27@yahoo.com>
2022-11-07 22:12:35 +0200

    feat: add dynamic link to games/:id, and use the id in the component

---
+++ src/views/GameDetails.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  id: string;
+}>();
+</script>
+
+<template>
+  <main>
+    <h1>Best Game Ever</h1>
+    <p>{{ $route.params.id }}</p>
+    <p>{{ props.id }}</p>
+  </main>
+</template>
