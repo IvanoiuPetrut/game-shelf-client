29371c91fff2f4f6f80547cc6a0760f4dec73433
Petrut <ivanoiu.petrut27@yahoo.com>
2022-11-07 22:32:41 +0200

    feat: add api call for the specific game id

--- src/views/GameDetails.vue
+++ src/views/GameDetails.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
+import { ref, onBeforeMount } from "vue";
+import axios from "axios";
+import { API_URL } from "@/api";
+
 const props = defineProps<{
   id: string;
 }>();
+
+const game = ref({});
+
+const fetchGame = async () => {
+  axios.get(`${API_URL}/${props.id}`).then((res) => {
+    game.value = res.data;
+    console.log(res.data);
+  });
+};
+
+onBeforeMount(() => {
+  fetchGame();
+});
 </script>
 
 <template>
