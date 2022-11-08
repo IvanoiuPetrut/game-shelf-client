c3b3b240976f2490c3c63233f14c30f6be8b09bb
Petrut <ivanoiu.petrut27@yahoo.com>
2022-11-08 19:43:55 +0200

    feat(style): add form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -2,44 +2,111 @@
 
 <template>
   <main>
-    <h1>Start using GameShelf 😎</h1>
-    <form @submit.prevent="register" class="form">
-      <div class="form__field">
-        <label for="nickname" class="form__label">Nickname</label>
-        <input
-          type="text"
-          class="form__input"
-          id="nickname"
-          v-model="nickname"
-        />
-      </div>
-      <div class="form__field">
-        <label for="email" class="form__label">E-mail address</label>
-        <input type="text" class="form__input" id="email" v-model="email" />
-      </div>
-      <div class="form__field">
-        <label for="password" class="form__label">Password</label>
-        <input
-          type="password"
-          class="form__input"
-          id="password"
-          v-model="password"
-        />
-      </div>
-      <div class="form__field">
-        <label for="password2" class="form__label">Confirm Password</label>
-        <input
-          type="password"
-          class="form__input"
-          id="password2"
-          v-model="password2"
-        />
-      </div>
-      <div class="form__field">
-        <button type="submit">Register</button>
-      </div>
-    </form>
+    <div class="register">
+      <h1>Start using GameShelf 😎</h1>
+      <form @submit.prevent="register" class="form">
+        <div class="form__field">
+          <label for="nickname" class="form__label">Nickname</label>
+          <input
+            type="text"
+            class="form__input"
+            id="nickname"
+            v-model="nickname"
+          />
+        </div>
+        <div class="form__field">
+          <label for="email" class="form__label">E-mail address</label>
+          <input type="text" class="form__input" id="email" v-model="email" />
+        </div>
+        <div class="form__field">
+          <label for="password" class="form__label">Password</label>
+          <input
+            type="password"
+            class="form__input"
+            id="password"
+            v-model="password"
+          />
+        </div>
+        <div class="form__field">
+          <label for="password2" class="form__label">Confirm Password</label>
+          <input
+            type="password"
+            class="form__input"
+            id="password2"
+            v-model="password2"
+          />
+        </div>
+        <button type="submit" class="btn form__btn">Register</button>
+      </form>
+    </div>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "@/assets/style/colors.scss" as colors;
+
+.register {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 1rem;
+  margin-top: 3.2rem;
+}
+
+h1 {
+  font-size: 1.6rem;
+  font-weight: bold;
+  margin-bottom: 1.6rem;
+
+  @media (min-width: 600px) {
+    font-size: 2.4rem;
+  }
+}
+
+.form {
+  background-color: colors.$neutral-bg-secondary;
+  padding: 1.2rem 1.8rem;
+  border-radius: 11px;
+  width: min(90%, 32rem);
+
+  @media (min-width: 600px) {
+    padding: 2.4rem 3.2rem;
+  }
+
+  &__field {
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+    margin-bottom: 1.2rem;
+  }
+
+  &__label {
+    font-size: 1.2rem;
+    color: colors.$neutral-text-secondary;
+  }
+
+  &__input {
+    padding: 0.4rem 0.8rem;
+    border: 1px solid colors.$neutral-text-secondary;
+    background-color: colors.$neutral-bg-secondary;
+    border-radius: 7px;
+    font-size: 1.4rem;
+    color: currentColor;
+
+    &:focus {
+      outline: none;
+      border-color: colors.$primary;
+    }
+  }
+
+  &__btn {
+    width: 100%;
+    margin-top: 1.2rem;
+    padding: 1rem 0;
+    font-size: 1rem;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+}
+</style>
