6e9a597ea63cb7d55d47d23b17c397c5e8e579a4
Petrut <ivanoiu.petrut27@yahoo.com>
2022-12-07 13:53:02 +0200

    feat: add watcher for game name query to fetch games

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
+import axios from "axios";
+import { API_URL } from "@/api";
+
 const props = defineProps<{
   category: string;
 }>();
+
+const gameQuery = ref("");
+const games = ref<any[]>([]);
+
+const fetchGames = async () => {
+  axios
+    .get(API_URL, {
+      params: {
+        search: gameQuery.value,
+        genres: "action",
+      },
+    })
+    .then((response) => {
+      console.log(response.data.results);
+      games.value = response.data.results;
+      // console.log(games.value);
+    });
+};
+
+watch(gameQuery, () => {
+  console.log("gameQuery changed");
+  fetchGames();
+});
 </script>
 
 <template>
-  <h1>Categories</h1>
-  <p>{{ props.category }}</p>
+  <p>Showing {3821} games</p>
+  <input type="text" v-model="gameQuery" />
+  <ul>
+    <li>Genres {{ props.category }}</li>
+    <li v-if="gameQuery.length > 0">Results for {{ gameQuery }}</li>
+  </ul>
+  <div class="games">
+    <p v-for="game in games" :key="game.id">{{ game.name }}</p>
+  </div>
 </template>
