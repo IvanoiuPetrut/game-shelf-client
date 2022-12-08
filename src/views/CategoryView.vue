07bfd501be8cc70be85997dfe058a6d7f5acac94
Petrut <ivanoiu.petrut27@yahoo.com>
2022-12-08 11:01:54 +0200

    feat: add game filters based on genres and tags. When filtering for checked parameters to make the API call if none is found in the list will return null as param

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -11,7 +11,6 @@ const props = defineProps<{
 
 const gameQuery = ref("");
 const games = ref<any[]>([]);
-const testLabel = ref(true);
 const platforms = ref<any[]>([
   { id: 4, name: "PC", checked: true },
   { id: 1, name: "Xbox", checked: false },
@@ -22,17 +21,48 @@ const platforms = ref<any[]>([
   { id: 18, name: "Playstation 4", checked: false },
   { id: 16, name: "Playstation 3", checked: false },
 ]);
+const genres = ref<any[]>([
+  { id: 4, name: "Action", checked: false },
+  { id: 51, name: "Indie", checked: false },
+  { id: 3, name: "Adventure", checked: false },
+  { id: 5, name: "RPG", checked: false },
+  { id: 10, name: "Strategy", checked: false },
+  { id: 2, name: "Shooter", checked: false },
+  { id: 40, name: "Casual", checked: false },
+  { id: 7, name: "Puzzle", checked: false },
+  { id: 11, name: "Arcade", checked: false },
+  { id: 83, name: "Platformer", checked: false },
+  { id: 1, name: "Racing", checked: false },
+  { id: 15, name: "Sports", checked: false },
+]);
+const tags = ref<any[]>([
+  { id: 31, name: "Singleplayer", checked: false },
+  { id: 7, name: "Multiplayer", checked: false },
+  { id: 13, name: "Atmosferic", checked: false },
+  { id: 42, name: "Great Soundtrack", checked: false },
+  { id: 18, name: "Co-op", checked: false },
+]);
 
 const fetchGames = async () => {
   axios
     .get(API_URL, {
       params: {
         search: gameQuery.value,
-        genres: props.category,
-        platforms: platforms.value
-          .filter((platform) => platform.checked)
-          .map((platform) => platform.id)
-          .join(","),
+        genres:
+          genres.value
+            .filter((genre) => genre.checked)
+            .map((genre) => genre.id)
+            .join(",") || null,
+        platforms:
+          platforms.value
+            .filter((platform) => platform.checked)
+            .map((platform) => platform.id)
+            .join(",") || null,
+        tags:
+          tags.value
+            .filter((tag) => tag.checked)
+            .map((tag) => tag.id)
+            .join(",") || null,
       },
     })
     .then((response) => {
@@ -41,13 +71,8 @@ const fetchGames = async () => {
     });
 };
 
-// watch(gameQuery, () => {
-//   fetchGames();
-// });
-
-//watch gameQuery and platforms
 watch(
-  [gameQuery, platforms],
+  [gameQuery, platforms, genres, tags],
   () => {
     fetchGames();
   },
@@ -63,15 +88,36 @@ watch(
     <li>Genres {{ props.category }}</li>
     <li v-if="gameQuery.length > 0">Results for {{ gameQuery }}</li>
   </ul>
-  {{ testLabel }}
-  <GameFilters label="Action" v-model="testLabel" is></GameFilters>
 
-  <GameFilters
-    v-for="platform in platforms"
-    :key="platform.id"
-    :label="platform.name"
-    v-model="platform.checked"
-  ></GameFilters>
+  <div>
+    <p>Tags</p>
+    <GameFilters
+      v-for="tag in tags"
+      :key="tag.id"
+      :label="tag.name"
+      v-model="tag.checked"
+    ></GameFilters>
+  </div>
+
+  <div>
+    <p>Genres</p>
+    <GameFilters
+      v-for="genre in genres"
+      :key="genre.id"
+      :label="genre.name"
+      v-model="genre.checked"
+    ></GameFilters>
+  </div>
+
+  <div>
+    <p>Platforms</p>
+    <GameFilters
+      v-for="platform in platforms"
+      :key="platform.id"
+      :label="platform.name"
+      v-model="platform.checked"
+    ></GameFilters>
+  </div>
   <div class="games">
     <router-link
       v-for="game in games"
