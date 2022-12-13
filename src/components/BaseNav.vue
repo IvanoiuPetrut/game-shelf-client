e06236a1af6b27f51d5d63aed05e43cc266c60a1
Petrut <ivanoiu.petrut27@yahoo.com>
2022-12-13 22:12:34 +0200

    feat: add BaseNav component

---
+++ src/components/BaseNav.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <router-link :to="{ name: 'register' }">Register</router-link>
+    <router-link :to="{ name: 'login' }">Login</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link>
+  </nav>
+</template>
