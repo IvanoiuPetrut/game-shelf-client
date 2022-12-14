b88aadbc7d4de97d52d0458b6778020c87ea8732
Petrut <ivanoiu.petrut27@yahoo.com>
2022-12-14 12:32:55 +0200

    feat(style): add style for nav on desktops

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import { onBeforeMount } from "vue";
 import { useGamesTopCriticsStore } from "@/stores/games-top-critics";
 import HeaderItem from "./components/HeaderItem.vue";
@@ -24,36 +24,4 @@ onBeforeMount(() => {
 
 <style lang="scss" scoped>
 @use "@/assets/style/colors.scss" as colors;
-
-// nav {
-//   width: 100%;
-//   font-size: 1.2rem;
-//   text-align: center;
-//   background-color: colors.$neutral-bg-secondary;
-//   padding: 1rem 0;
-//   margin-bottom: 1.6rem;
-//   @media (min-width: 768px) {
-//     padding: 1.6rem 0;
-//     margin-bottom: 3.2rem;
-//   }
-// }
-
-nav a.router-link-exact-active {
-  color: colors.$accent;
-  font-weight: bold;
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid colors.$neutral-bg;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 </style>
--- src/components/BaseNav.vue
+++ src/components/BaseNav.vue
@@ -4,32 +4,55 @@ import IconBook from "./icons/IconBook.vue";
 
 <template>
   <nav class="nav">
-    <RouterLink to="/"> <IconBook /> GameShelf </RouterLink>
-    <router-link :to="{ name: 'register' }">Register</router-link>
-    <!-- <router-link :to="{ name: 'login' }">Login</router-link> -->
-    <router-link :to="{ name: 'about' }">About</router-link>
+    <div class="nav__left">
+      <RouterLink to="/" class="nav__logo"> <IconBook /> GameShelf </RouterLink>
+    </div>
+    <div class="nav__right">
+      <router-link :to="{ name: 'category', params: { category: 'default' } }">
+        Categories
+      </router-link>
+      <router-link :to="{ name: 'about' }">About</router-link>
+      <router-link :to="{ name: 'register' }">Register</router-link>
+      <!-- <router-link :to="{ name: 'login' }">Login</router-link> -->
+    </div>
   </nav>
 </template>
 
 <style lang="scss" scoped>
 @use "@/assets/style/colors.scss" as colors;
 .nav {
-  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1.6rem;
   background-color: colors.$neutral-bg-secondary;
-  padding: 1rem 0;
+  width: 100%;
+  padding: 1rem 10.8rem;
+  margin-bottom: 6.4rem;
+
+  &__logo {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+  }
+
+  &__right {
+    display: flex;
+    gap: 1.6rem;
+  }
 }
 
-nav a.router-link-exact-active {
+.nav a.router-link-exact-active {
   color: colors.$accent;
 }
 
-nav a.router-link-exact-active:hover {
-  // background-color: colors.$accent-transparent;
+.nav__left a:first-of-type {
+  color: colors.$primary;
 }
 
-nav a {
+.nav a {
   display: inline-block;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
   padding: 0.4rem 0.8rem;
   // border-left: 1px solid colors.$neutral-bg;
   font-weight: bold;
@@ -38,9 +61,5 @@ nav a {
   &:hover {
     background-color: colors.$neutral-bg;
   }
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 </style>
